aba75399c18bd901e7e2d91e8f51e28f263c0713
dpburgess <dpburgess88@gmail.com>
2015-08-16 16:09:35 -0400

    first version of completed project3.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,40 @@
+var rightEdge = 400;
+var leftEdge = 0;
+var topEdge = 70;
+var bottomEdge = 385;
+var victoryLine = 60;
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.width = 75;
+    this.height = 75;
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    //var obj = Object.create(Enemy.prototype);
+    //obj.update = Enemy.prototype.update;
+    //obj.render = Enemy.prototype.render;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-}
+    //return obj;
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    if (this.x < 504) {
+    this.x += this.speed * dt;
+    }else{
+    this.x = -100;
+    };
+};
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -25,12 +45,73 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 75;
+    this.height = 75;
+    this.score = 0;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 385;
+};
+
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyCode) {
+    switch (keyCode) {
+        case "left":
+        if(this.x > leftEdge) {
+            this.x = this.x - 100;
+        }
+            break;
+        case "right":
+        if(this.x < rightEdge) {
+            this.x = this.x + 100;
+        }
+            break;
+        case "up":
+        if(this.y > topEdge) {
+            this.y = this.y - 83;
+        }
+            break;
+        case "down":
+        if(this.y < bottomEdge) {
+            this.y = this.y + 83;
+        }
+            break;
+    }
+};
+
+Player.prototype.update = function(dt){
+
+};
+
+Player.prototype.score = function(){
+    var ctxx = canvas.getContext("2d");
+        ctxx.fillStyle = "blue";
+        ctxx.beginPath()
+        ctxx.moveTo(100, 100);
+        ctxx.LineTo(250, 250);
+        ctxx.fill();
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var enemy1 = new Enemy(-100, 60, 75);
+var enemy2 = new Enemy(-100, 219, 50);
+var enemy3 = new Enemy(-100, 136, 100);
 
+var allEnemies = [enemy1, enemy2, enemy3];
+var player = new Player(200, 385);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -44,3 +125,16 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+var checkCollisions = function(){
+    for(var i = 0; i < allEnemies.length; i++){
+        if(player.y === Math.floor(allEnemies[i].y)){
+            if ((Math.floor(allEnemies[i].x) + allEnemies[i].width) > player.x &&
+                player.x + player.width > Math.floor(allEnemies[i].x) &&
+                player.y < (Math.floor(allEnemies[i].y) + allEnemies[i].height) &&
+                player.y + player.height > Math.floor(allEnemies[i].y)){
+                player.reset();
+            }
+        }
+    }
+};
--- js/engine.js
+++ js/engine.js
@@ -80,7 +80,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisions();
     }
 
     /* This is called by the update function  and loops through all of the
@@ -135,7 +135,10 @@ var Engine = (function(global) {
                 ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
             }
         }
-
+        //ctx.fillStyle = "#FF0000";
+        //ctx.fillRect(420,55,60,70)
+        ctx.font = "30px Arial";
+        ctx.fillText("0", 420, 50);
 
         renderEntities();
     }
@@ -153,6 +156,9 @@ var Engine = (function(global) {
         });
 
         player.render();
+        if (player.y === 53) {
+            setTimeout(player.reset(),1000);
+        }
     }
 
     /* This function does nothing but it could have been a good place to
@@ -160,7 +166,7 @@ var Engine = (function(global) {
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-        // noop
+
     }
 
     /* Go ahead and load all of the images we know we're going to need to
@@ -182,3 +188,4 @@ var Engine = (function(global) {
      */
     global.ctx = ctx;
 })(this);
+
